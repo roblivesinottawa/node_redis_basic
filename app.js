9be2d7e0c812e092600b39b1e6f46cf1fe8b4688
Robson Trajano <59901422+roblivesinottawa@users.noreply.github.com>
2021-05-20 09:06:00 -0400

    added examples

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const redis = require('redis')
-const client = redis.createClient()
+const redis = require("redis");
 
-client.on('connect', () => console.log('connected to redis!'))
+const client = redis.createClient();
+
+client.set("framework", "Node JS", () => console.log(reply));
+client.get("framework", (err, reply) => console.log(reply));
+client.hmset(
+  "frameworks",
+  "javascript",
+  "AngularJS",
+  "css",
+  "Bootstrap",
+  "node",
+  "Express",
+);
+client.hgetall("frameworks", (err, object) => console.log(object));
+
+client.on("connect", () => console.log("connected to redis!"));
